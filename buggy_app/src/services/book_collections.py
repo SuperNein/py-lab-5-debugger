ec2dec0def757beb1740be20250613a42f6f8c22
Konstantin Smage <kpsmage@mai.education>
2025-12-24 22:35:56 +0300

    fix: fix bug_3 and make comment

--- buggy_app/src/services/book_collections.py
+++ buggy_app/src/services/book_collections.py
@@ -87,8 +87,8 @@ class IndexDict:
     def __generate_dict(self) -> dict[str, BookCollection]:
         """Generate actual dictionary of collection"""
         res: dict[str, BookCollection] = dict()
-        # books = self.__items.copy()
-        books = self.__items
+        # books = self.__items  # Bug 3
+        books = self.__items.copy()
         while books:
             book = books.pop()
             key = getattr(book, self.index)
